bba6b4f4a7d66708254963ff26871bf3687ffe81
Carlos Donatucci <30753327+cfdonatucci@users.noreply.github.com>
2020-08-08 18:53:10 -0300

    Create HELLOW.cbl

---
+++ source/cobol/HELLOW.cbl
@@ -0,0 +1,24 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. HELLOW.
+      *////////////////////////////////////////////////////////////-*
+       ENVIRONMENT DIVISION.
+       INPUT-OUTPUT SECTION.
+       DATA DIVISION.
+      *////////////////////////////////////////////////////////////-*
+       WORKING-STORAGE SECTION.
+       77  FS-FILER      PIC XX    VALUE ZEROS.
+       77  RETCODE       PIC S9(9) COMP VALUE +0.
+       77  CURRENT-TIME  PIC 9(8).
+       77  WS-VAR1       PIC X(4) VALUE '0060'.
+       77  WS-VAR2       PIC X(5) VALUE SPACES.
+       77  WS-EXAMPLE    PIC XX.
+      *////////////////////////////////////////////////////////////-*
+       PROCEDURE DIVISION.
+      *////////////////////////////////////////////////////////////-*
+       MAIN  SECTION.
+      
+            DISPLAY 'HELLOW Camilo, Julio.'.
+            GOBACK.
+      
+       END-MAIN. EXIT.
+      *////////////////////////////////////////////////////////////-*
