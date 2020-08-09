475690d6498e0d5f903696992fec2b3096e0500f
CARLOSFRANCISCODONAT <donatucc@ar.ibm.com>
2020-08-09 01:37:32 -0300

    example 3

--- source/cobol/HELLOW.cbl
+++ source/cobol/HELLOW.cbl
@@ -12,6 +12,7 @@
        77  WS-VAR1       PIC X(4) VALUE '0060'.
        77  WS-VAR2       PIC X(5) VALUE SPACES.
        77  WS-EXAMPLE2   PIC XX.
+       77  WS-EXAMPLE3   PIC XX.
       *////////////////////////////////////////////////////////////-*
        PROCEDURE DIVISION.
       *////////////////////////////////////////////////////////////-*
