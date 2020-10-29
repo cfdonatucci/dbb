eafcd0f13f6dee6fd725d280a4bb1ffa157899f9
CarlosFranciscoDonat <CarlosFranciscoDonat@DESKTOP-L9R6C8C.mshome.net>
2020-10-29 17:02:02 -0300

    Dan Commit

--- appName/cobol/ejemplo.cbl
+++ appName/cobol/ejemplo.cbl
@@ -3,6 +3,6 @@
        IDENTIFICATION DIVISION.
        PROGRAM-ID. HELLO.
        PROCEDURE DIVISION.
-           DISPLAY 'Hola  world Prisma bitbucket'.
+           DISPLAY 'Hola  world Dan bitbucket'.
            STOP RUN.
       */////////////////////////////////////////////
